d58a22a6c3ae65b2fe7803625ca8db01628ee486
Vithyashagar <vithyashagar98@gmail.com>
2021-05-13 14:32:57 +0530

    First Commit

---
+++ Lab10/src/main/java/com/item.java
@@ -0,0 +1,197 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class item {
+	
+	private static Statement stmt = null;
+	static int id = 0;
+	
+	public Connection connect(){
+		
+		Connection con = null;
+		
+		try{
+				
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root", "M@ng@th@9093");
+
+			//For Testing
+			System.out.println("Successfully connected");
+			
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public String insertItem(String code, String name, String price, String desc) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			if(con == null) {
+				
+				return "Error while connecting to Database";
+				
+			}
+			
+			//Create Prepared Statement
+			String query = "INSERT INTO item(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`) VALUES(?,?,?,?,?)" ;
+			
+			PreparedStatement preparedStmt  = con.prepareStatement(query);
+			
+			//binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Inserted Succesfully";
+			
+			
+		}catch(Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	public String readItems()
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{
+	return "Error while connecting to the database for reading.";
+	}
+	// Prepare the html table to be displayed
+	output = "<table border=\"1\"><tr><th>Item Code</th><th>Item Name</th><th>Item Price</th><th>Item Description</th><th>Update</th><th>Remove</th></tr>";
+	String query = "select * from item";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	// iterate through the rows in the result set
+	while (rs.next())
+	{
+	String itemID = Integer.toString(rs.getInt("itemID"));
+	String itemCode = rs.getString("itemCode");
+	String itemName = rs.getString("itemName");
+	String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+	String itemDesc = rs.getString("itemDesc");
+	// Add into the html table
+	output += "<tr><td><input id='hidItemIDUpdate'name='hidItemIDUpdate'type='hidden' value='" + itemID + "'>"
+	+ itemCode + "</td>";
+	output += "<td>" + itemName + "</td>";
+	output += "<td>" + itemPrice + "</td>";
+	output += "<td>" + itemDesc + "</td>";
+	// buttons
+	output += "<td><input name='btnUpdate' type='button' value='Update'class=' btnUpdate btn btn-secondary'></td><td><form method='post' action='items.jsp'><input name='btnRemove' type='submit'value='Remove' class='btn btn-danger'><input name='hidItemIDDelete' type='hidden'value='" + itemID + "'>" + "</form></td></tr>";
+	}
+	con.close();
+	// Complete the html table
+	output += "</table>";
+	}
+	
+	catch (Exception e)
+	{
+	output = "Error while reading the items.";
+	System.err.println(e.getMessage());
+	}
+	return output;
+	}
+	
+	public String deleteData(String itemId) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			if(con == null) {
+				
+				return "Error while connecting to Database";
+				
+			}
+			
+			//create prepared statement
+			String query = "delete from item where itemId = ? ";
+			
+			PreparedStatement preparedStmt  = con.prepareStatement(query);
+			
+			preparedStmt.setInt(1, Integer.valueOf(itemId));
+			
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Item Deleted";
+			
+			
+		}catch(Exception e) {
+			output = "Error while Deleting";
+			System.err.println(e.getMessage());
+			
+		}
+		
+		return output;
+	}
+	
+	public String updateItem(String itemID, String itemCode, String itemName, String itemPrice, String itemDesc)  
+	{
+		String output = "";	
+		try {
+						
+			Connection con = connect();
+		
+			
+			if (con == null){
+				return "Error while connecting to the database for reading.";
+			}
+
+			stmt = con.createStatement();
+						
+			String update="UPDATE test.item"
+					+ " SET itemCode='"+itemCode+"',"
+							+ "itemName= '"+itemName+"',"
+							+ "itemPrice = '"+itemPrice+"',"
+							+ "itemDesc = '"+itemDesc+"'"
+					+ " WHERE itemId = '"+itemID+"'";
+			
+			int rsv = stmt.executeUpdate(update);
+			
+
+			if(rsv > 0) {
+				output = "Updated successfully";
+			}
+			con.close();
+			
+			
+			
+		}catch(Exception e) {
+			output = "Error while updating";
+			System.err.println(e.getMessage());
+		}
+		return output;
+		
+		
+	}
+	
+	
+}
---
+++ Lab10/src/main/webapp/items.jsp
@@ -0,0 +1,81 @@
+<%@page import = "com.item"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+
+<%	
+	//Save---------------------------------
+	if (request.getParameter("itemCode") != null){
+		item itemObj = new item();
+		String stsMsg = "";
+	
+		//Insert--------------------------
+		if (request.getParameter("hidItemIDSave") == ""){
+			stsMsg = itemObj.insertItem(request.getParameter("itemCode"),
+			request.getParameter("itemName"),
+			request.getParameter("itemPrice"),
+			request.getParameter("itemDesc"));
+		}
+		
+		//Update----------------------
+		else{
+			stsMsg = itemObj.updateItem(request.getParameter("hidItemIDSave"),
+			request.getParameter("itemCode"),
+			request.getParameter("itemName"),
+			request.getParameter("itemPrice"),
+			request.getParameter("itemDesc"));
+		}
+			session.setAttribute("statusMsg", stsMsg);
+	}
+	
+	//Delete-----------------------------
+	if (request.getParameter("hidItemIDDelete") != null){
+		item itemObj = new item();
+		String stsMsg =
+		itemObj.deleteData(request.getParameter("hidItemIDDelete"));
+		session.setAttribute("statusMsg", stsMsg);
+	}
+
+%>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name = "viewport" content="width=device-width, initial-scale=1" >
+		<title>Items Management</title>
+		<link href="Views/bootstrap.min.css" rel="stylesheet">
+		<script src="components/jquery-3.4.0.min.js" type ="text/javascript"></script>
+		<script src="components/items.js" type ="text/javascript"></script>
+	</head>
+	<body>
+		<div class ="container">
+			<div class ="row">
+				<div class ="col">							
+					<h1>Items Management</h1>
+					
+					<form id="formItem" name="formItem" method="post" action="items.jsp">
+						Item code: <input id="itemCode" name="itemCode" type="text" class="form-control form-control-sm"><br>
+						Item name: <input id="itemName" name="itemName" type="text" class="form-control form-control-sm"><br>
+						Item price: <input id="itemPrice" name="itemPrice" type="text" class="form-control form-control-sm"><br>
+						Item description: <input id="itemDesc" name="itemDesc" type="text" class="form-control form-control-sm"><br>
+						<input id="btnSave" name="btnSave" type="button" value="Save" class="btn btn-primary">
+						<input type="hidden" id="hidItemIDSave" name="hidItemIDSave" value="">
+					</form>
+					
+					<!-- Alert Messages -->
+					<div id="alertSuccess" class="alert alert-success"></div>
+					<div id="alertError" class="alert alert-danger"></div>
+					
+					<br>
+					<%
+						item itemObj = new item();
+						out.print(itemObj.readItems());
+					%>			
+				</div>
+			</div>
+		</div>
+	</body>
+
+	
+</html>
